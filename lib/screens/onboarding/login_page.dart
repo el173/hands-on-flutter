3e38eb4f4c220d422c9a7f53259a756abc055cad
Hashith Karunarathne <hashithkarunarathne@live.com>
2020-01-07 20:28:08 +0530

    Added basic project

---
+++ lib/screens/onboarding/login_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _email;
+
+  onPressed () {
+    print("$_email $_password");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page Flutter Firebase"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              Text(
+                'Login Information',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  onSaved: (value) => _email = value,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: "Email Address")),
+              TextFormField(
+                  onSaved: (value) => _password = value,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: "Password")),
+              SizedBox(height: 20.0),
+              RaisedButton(child: Text("LOGIN"), onPressed: onPressed(),),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
